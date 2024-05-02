c215a8951f06c9a54eb4a1a1f89f62836f9fabd8
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-03 02:18:02 +0500

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,1 +1,27 @@
+import random
+
+
+def first_num():
+    numbers = range(3,21)
+    rock = random.choice(numbers)
+    return rock
+
+
+rock = first_num()
+print(rock)
+
+result = []
+
+for i in range(1, rock):
+    for k in range(1, rock):
+        sum = i + k
+        if i == k:
+            continue
+        if rock % sum == 0:
+            list_3 = [i, k]
+            if [k, i] in result:
+                continue
+            result.append(list_3)
+
+print(*result)
 
