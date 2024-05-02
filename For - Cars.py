9f99d497b64abf1115ef87599c432f7fb500f184
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-02 17:49:38 +0500

    Update For - Cars.py

--- For - Cars.py
+++ For - Cars.py
@@ -2,6 +2,5 @@ list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for i in list_:
     print('Я езжу на автомобиле марки ' + i)
-    for k in range(len(list_)):
-        cars_count += 10
-        print(cars_count)
+    cars_count += 10
+    print(cars_count)
