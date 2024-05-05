62ec58564d3b63702875f1acf870618c6d9328ef
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-05 20:34:43 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
+
+
+def test():
+    a = 1
+    b = 2
+    print(a, b)
+
+
+test()
+
+
+def test2(a=3, b=4, c=5):
+    print(a, b)
+
+
+test2()
